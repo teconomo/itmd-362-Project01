c847feb4248fd2f45d52c2c49b6bc38aee5a8d06
Theo Economos <theoeconomos1@gmail.com>
2017-03-03 22:08:11 -0600

    working on js errors

--- js/site.js
+++ js/site.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
-	$("#fitnessform").on('submit', function (e) {
-		e.preventDefault();
-	})
-})
 
-function myGreeting() {
-	document.getElementById("submission").innerHTML = "Congratulations! Welcome to the Team";
-}
+  $("#fitnessform").on('submit', function (e) {
+  	e.preventDefault();
+	});
+
+  $(#fitnessform).on('submit', function (e) {
+  	document.getElementById("submission").innerHTML = "Thank you!";
+  });
+ });
+
+
